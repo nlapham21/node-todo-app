41d2c5847b4135cd0d2a7afe0c3f1fc70c3935e7
Nolan Lapham <nlapham21@gmail.com>
2018-12-08 16:58:43 -0500

    hash user password before saving

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,15 +1,34 @@
 const {SHA256} = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
-var data = {
-    id: 10
-};
+var password = '123abc!';
 
-var token = jwt.sign(data, '123abc');
-console.log(token);
+// bcrypt.genSalt(10, (err, salt) => {
+//     bcrypt.hash(password, salt, (err, hash) => {
+//         console.log(hash);
+//     });
+// });
 
-var decoded = jwt.verify(token, '123abc');
-console.log('Decoded: ', decoded);
+var hashedPassword = '$2a$10$T388K1MjsloQcJ046D2PVOGVfB.E5xciK0oJzdPDQXSEw6A0WWixW';
+
+bcrypt.compare(password, hashedPassword, (err, success) => {
+    console.log(success);
+});
+
+
+///////////////////////////////////////////////////
+
+// var data = {
+//     id: 10
+// };
+
+// var token = jwt.sign(data, '123abc');
+// console.log(token);
+
+// var decoded = jwt.verify(token, '123abc');
+// console.log('Decoded: ', decoded);
+///////////////////////////////////////////////////
 
 // var message = 'I am user number 21';
 // var hash = SHA256(message).toString();
