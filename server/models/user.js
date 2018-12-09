85d9bfd5ac90ba761c08c37a6518cbd421a7d996
Nolan Lapham <nlapham21@gmail.com>
2018-12-08 22:19:09 -0500

    add POST /users/login route

--- server/models/user.js
+++ server/models/user.js
@@ -69,6 +69,27 @@ UserSchema.statics.findByToken = function (token) {
     });
 };
 
+UserSchema.statics.findByCredentials = function (email, password) {
+    var User = this;
+
+    return User.findOne({email}).then((user) => {
+        if (!user) {
+            return Promise.reject();
+        }
+        
+        return new Promise((resolve, reject) => {
+            bcrypt.compare(password, user.password, (err, success) => {
+                if (success) {
+                    resolve(user);
+                }
+                reject();
+            });
+        });
+    }).catch((err) => {
+        
+    })
+};
+
 // middleware for mongoose
 // Use is to hash the password before we save it in mongoose
 UserSchema.pre('save', function (next) {
