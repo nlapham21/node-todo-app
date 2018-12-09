23f09a2191c7e66b156f6aa81b3ec2f3d38165ce
Nolan Lapham <nlapham21@gmail.com>
2018-12-08 22:53:57 -0500

    added tests for DELETE /users/me/token

--- server/models/user.js
+++ server/models/user.js
@@ -55,7 +55,7 @@ UserSchema.methods.generateAuthToken = function () {
 UserSchema.methods.removeToken = function (token) {
     var user = this;
 
-    return user.update({
+    return user.updateOne({
         $pull: { // $pull removes this from the user
             tokens: {
                 token: token
